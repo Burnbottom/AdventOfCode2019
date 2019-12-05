05e2831d94319ca56e15f01a238fa98c30d5629c
Margareta Vi <margaretavi@Margaretas-MacBook-Pro.local>
2019-12-05 17:13:24 +0100

    add rest of the days and renaming files

---
+++ 201901.py
@@ -0,0 +1,22 @@
+import math
+input = open("data01.txt", "r")
+
+def calc_fuel(data):
+    # divide by 3, round down, subtract 2
+    return (math.floor(int(data)/3)-2)
+
+total_fuel = 0
+for e in input:
+    done = False
+    module_fuel = 0
+    tmp = e
+    while not done:
+        tmp = calc_fuel(tmp)
+        if (tmp<0):
+            total_fuel += module_fuel
+            done = True
+        else:
+            module_fuel += tmp
+
+print("total fuel needed: " + str(int(total_fuel)))
+
---
+++ 201903-2.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+input = open("data3.txt", "r")
+mapOfWire = {}
+wire_id = 1
+
+def calculateManhattanDistance(intersectPoint):
+    return abs(intersectPoint[0] - 0) + abs(intersectPoint[1] - 0)
+
+def splitAction(action):
+    return (action[0], action[1:])
+
+def addOrUpdateMap(coord, wire_id, stepsTaken):
+    if coord in mapOfWire:
+        wireIdAndSteps = mapOfWire[coord]
+        wireIdAndSteps.update({wire_id: stepsTaken})
+    else:
+        wireIdAndSteps = {wire_id: stepsTaken}
+        mapOfWire[coord] = wireIdAndSteps
+
+def parseWire(wire):
+    stepsTaken = 0
+    x_coord = 0
+    y_coord = 0
+    actions = wire.split(",")
+    for action in actions:
+        act = splitAction(action)
+        direction = act[0]
+        steps = act[1]
+        if direction == 'R': # Right
+            # print("going right")
+            startX = x_coord
+            for _ in range(startX , startX + int(steps)):
+                stepsTaken += 1
+                x_coord += 1
+                coord = (x_coord,y_coord)
+                addOrUpdateMap(coord, wire_id, stepsTaken)
+
+        if direction == 'U': # Up
+            # print("going up")
+            startY = y_coord
+            for _ in range(startY, startY + int(steps)):
+                stepsTaken += 1
+                y_coord += 1
+                coord = (x_coord,y_coord)
+                addOrUpdateMap(coord, wire_id, stepsTaken)
+
+        if direction == 'L': # Left
+            # print("going left")
+            startX = x_coord
+            for _ in range(startX, x_coord - int(steps), -1):
+                stepsTaken += 1
+                x_coord -= 1
+                coord = (x_coord,y_coord)
+                addOrUpdateMap(coord, wire_id, stepsTaken)
+
+        if direction == 'D': # Down
+            # print("going down")
+            startY = y_coord
+            for _ in range(startY, y_coord - int(steps), -1):
+                stepsTaken += 1
+                y_coord -= 1
+                coord = (x_coord,y_coord)
+                addOrUpdateMap(coord, wire_id, stepsTaken)
+
+for wire in input:
+    wire = wire.strip('\n')
+    parseWire(wire)
+    wire_id += 1
+
+intersectingCoordinates = set()
+listOfSteps = []
+for k , v in mapOfWire.items():
+    if len(v) > 1:
+        intersectingCoordinates.add(k)
+        listOfSteps.append(v)
+
+# print("intersecing coordinates" + str(intersectingCoordinates))
+shortestManhattanDistance = np.inf
+for point in intersectingCoordinates:
+    mhd = calculateManhattanDistance(point)
+    if mhd < shortestManhattanDistance:
+        shortestManhattanDistance = mhd
+
+print(f"the shortest manhattan distance is: {shortestManhattanDistance}")
+
+# print(f"list of steps: {listOfSteps}")
+fewestSteps = np.inf
+for elems in listOfSteps:
+    tmpSteps = elems[1] + elems[2]
+    if tmpSteps < fewestSteps:
+        fewestSteps = tmpSteps
+
+print(f"fewest steps to collision is: {fewestSteps}")
